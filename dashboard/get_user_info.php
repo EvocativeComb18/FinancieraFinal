6a02812525efaae9f5591b3087756e18f20b2259
Fernando Gael Mendoza Cortés <fernandogaelmendozacortes@gmail.com>
2024-09-01 12:39:28 -0600

    Borrar todos los archivos

--- dashboard/get_user_info.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-header('Content-Type: application/json');
-
-$user = isset($_SESSION['user']) ? $_SESSION['user'] : null;
-echo json_encode($user);
-?>
--- dashboard/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_destroy();
-header("Location: https://droopystone36.org.uk/test3/dashboard/pages-login.html");
-exit();
-?>
